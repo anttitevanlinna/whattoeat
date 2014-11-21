ff426b56a381c44f29103ef77b232b38f59bcd36
anttitevanlinna <tevanlin@gmail.com>
2014-11-21 03:58:01 +0000

    more foods

--- foods.js
+++ foods.js
@@ -1,4 +1,4 @@
 module.exports = function(callback){
-  var foods = ['pasta', 'pizza', 'soup', 'nothing', 'just candy'];
+  var foods = ['pasta', 'pizza', 'soup', 'nothing', 'just candy','fettucine alfredo','ham and eggs', 'steak'];
   return foods[Math.floor(Math.random()*foods.length)];
 }
