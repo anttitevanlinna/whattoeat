8e6b2b346f41f9695983d259633f196760ad17d4
anttitevanlinna <tevanlin@gmail.com>
2014-11-21 00:04:20 +0000

    use random food

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express')
+var foods = require('./foods̈́');
 var app = express();
+
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send( foods() );
 })
 
 app.listen(app.get('port'), function() {
