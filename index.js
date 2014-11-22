a46e059d9c45a2a5492a939ee10f146693cb37f7
anttitevanlinna <tevanlin@gmail.com>
2014-11-22 04:05:32 +0000

    moved api, intial index file

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-var server = require('./server');
-var app = server();
+var express = require('express');
+var foods = require('./foods');
+var app = express();
 
 
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
 
+app.get('/api/randomfood', function(request, response) {
+  response.send( foods() );
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
