9a89e384b689b1c846ed58c2fc6b405c80dd625e
anttitevanlinna <tevanlin@gmail.com>
2014-11-22 15:20:02 +0000

    initial angular setup

---
+++ public/js/main.js
@@ -0,0 +1,25 @@
+var foods = angular.module('foods', []);
+
+function mainController($scope, $http) {
+    $scope.formData = {};
+
+    $http.get('/api/food')
+        .success(function(data) {
+            $scope.currentfood = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    $scope.addFood = function() {
+        $http.post('/api/addfood', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {}; // clear 
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+}
